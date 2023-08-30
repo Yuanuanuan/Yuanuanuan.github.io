4df42777c120a68db3caca76f62528f8ff74032a
Yuanuanuan <asdll6618@gmail.com>
2023-08-30 13:16:09 +0800

    feat: loading view

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 
-  const header = document.querySelector('header')
-  const nav = document.querySelector('nav')
-  const title = document.querySelector(".title")
-  const titleScroll = document.querySelector('.title-scroll')
-  const menuToggle = document.querySelector('.menu-toggle')
-  const menuIcon = document.querySelector('.menu-icon')
-  const closeIcon = document.querySelector('.close-icon')
-  const list = document.querySelector('ul')
+const header = document.querySelector('header')
+const nav = document.querySelector('nav')
+const title = document.querySelector(".title")
+const titleScroll = document.querySelector('.title-scroll')
+const menuToggle = document.querySelector('.menu-toggle')
+const menuIcon = document.querySelector('.menu-icon')
+const closeIcon = document.querySelector('.close-icon')
+const list = document.querySelector('ul')
 
-  const bgImg = document.querySelector('.bg-img')
-  const bgImgWidth = bgImg.offsetWidth;
-  const bgImgHeight = bgImg.offsetHeight;
+const bgImg = document.querySelector('.bg-img')
+const bgImgWidth = bgImg.offsetWidth;
+const bgImgHeight = bgImg.offsetHeight;
 
-  const aboutMe = document.querySelector('#about-me')
-  const aboutMeImg = document.querySelectorAll('.about-me-img');
-  const aboutMeImg1 = document.querySelectorAll('.about-me-img1');
-  const aboutMeImg2 = document.querySelectorAll('.about-me-img2');
-  const aboutMeImg3 = document.querySelectorAll('.about-me-img3');
-  const aboutMeImg4 = document.querySelectorAll('.about-me-img4');
-  const aboutMeImg5 = document.querySelectorAll('.about-me-img5');
-  const aboutMeImg6 = document.querySelectorAll('.about-me-img6');
-  const aboutMeImg7 = document.querySelectorAll('.about-me-img7');
-  const aboutMeImg8 = document.querySelectorAll('.about-me-img8');
-  const aboutMeImg9 = document.querySelectorAll('.about-me-img9');
+const aboutMe = document.querySelector('#about-me')
+
+window.addEventListener('load', () => {
+  const loading = document.querySelector('#loading')
+  const loadSuccess = document.querySelector('#load-success')
+
+  setTimeout(() => {
+    loading.style.display = 'none';
+    loadSuccess.style.display = '1';
+  }, 2000);
+})
+
   
-
   let animation = gsap.to(titleScroll, { y: 20, duration: 1, repeat: -1 });
 
   window.addEventListener('scroll', () => {
@@ -80,9 +80,35 @@
       closeIcon.style.display = 'none';
     }
   });
+
+
+const LoadingAnimation = gsap.timeline({repeat: -1, repeatDelay: 0.5})
+
+LoadingAnimation
+  .to('.loading-title', {y:-30, duration: 0.2, ease: 'none'})
+  .to('.loading-title', {y:0, duration: 0.2, ease: 'none'})
+  .to('.loading-title1', {y:-30, duration: 0.2, ease: 'none'}, '<-0.15')
+  .to('.loading-title1', {y:0, duration: 0.2, ease: 'none'})
+  .to('.loading-title2', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title2', {y:0, duration: 0.2})
+  .to('.loading-title3', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title3', {y:0, duration: 0.2})
+  .to('.loading-title4', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title4', {y:0, duration: 0.2})
+  .to('.loading-title5', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title5', {y:0, duration: 0.2})
+  .to('.loading-title6', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title6', {y:0, duration: 0.2})
+  .to('.loading-title7', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title7', {y:0, duration: 0.2})
+  .to('.loading-title8', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title8', {y:0, duration: 0.2})
+  .to('.loading-title9', {y:-30, duration: 0.2}, '<-0.15')
+  .to('.loading-title9', {y:0, duration: 0.2})
+
   
   
-  const titleAnimation = gsap.timeline({repeat: -1, repeatDelay: 2});
+const titleAnimation = gsap.timeline({repeat: -1, repeatDelay: 2});
   
   titleAnimation
     .to(title, { opacity: 0, duration: 0.15, stagger: 0.05, repeat: 3})
@@ -93,16 +119,32 @@ const AboutMeAnimation = gsap.timeline({ paused: true});
 
 
 AboutMeAnimation
-  .to(aboutMeImg, {x:0, scale: 1, duration: .5})
-  .to(aboutMeImg1, {x:20, scale: 1.1, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg2, {x:40, scale: 1.2, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg3, {x:60, scale: 1.3, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg4, {x:80, scale: 1.4, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg5, {x:100, scale: 1.5, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg6, {x:120, scale: 1.6, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg7, {x:140, scale: 1.7, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg8, {x:160, scale: 1.8, transformOrigin: "center bottom"}, '<')
-  .to(aboutMeImg9, {x:180, scale: 1.9, transformOrigin: "center bottom"}, '<');
+  .to('.about-me-img', {x:0, scale: 1, duration: .5})
+  .to('.about-me-img1', {x:20, scale: 1.1, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img2', {x:40, scale: 1.2, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img3', {x:60, scale: 1.3, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img4', {x:80, scale: 1.4, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img5', {x:100, scale: 1.5, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img6', {x:120, scale: 1.6, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img7', {x:140, scale: 1.7, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img8', {x:160, scale: 1.8, transformOrigin: "center bottom"}, '<')
+  .to('.about-me-img9', {x:180, scale: 1.9, transformOrigin: "center bottom"}, '<')
+
+const ProfileAnimation = gsap.timeline({ paused: true })
+
+ProfileAnimation
+  .from('.profile-paragraph1', {y:30, opacity: 0, duration: 0.5})
+  .from('.profile-paragraph2', {y:30, opacity: 0, duration: 0.5})
+  .from('.profile-paragraph3', {y:30, opacity: 0, duration: 0.5})
+
+  ScrollTrigger.create({
+    trigger: "#about-me",
+    start: 'top 20%',
+    onEnter: () => {
+      ProfileAnimation.play();
+    }
+  })
+  
 
   ScrollTrigger.create({
     trigger: "#about-me",
